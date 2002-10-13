48f2dbb9ceeb93766952fc40d974135c9db9858f
hans <hans@954f8c5b-7b00-dc11-b283-0030488c597c>
2002-10-13 15:57:19 +0000

    - mention the GPL the right way - extend NaN Holding copyright to its proper startup
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@14 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/gameengine/GamePlayer/netscape/test/gears/TST_gears.c
+++ blender/source/gameengine/GamePlayer/netscape/test/gears/TST_gears.c
@@ -3,14 +3,24 @@
  *
  * ***** BEGIN GPL/BL DUAL LICENSE BLOCK *****
  *
- * The contents of this file may be used under the terms of either the GNU
- * General Public License Version 2 or later (the "GPL", see
- * http://www.gnu.org/licenses/gpl.html ), or the Blender License 1.0 or
- * later (the "BL", see http://www.blender.org/BL/ ) which has to be
- * bought from the Blender Foundation to become active, in which case the
- * above mentioned GPL option does not apply.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. The Blender
+ * Foundation also sells licenses for use in proprietary software under
+ * the Blender License.  See http://www.blender.org/BL/ for information
+ * about this.
  *
- * The Original Code is Copyright (C) 2002 by NaN Holding BV.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The Original Code is Copyright (C) 2001-2002 by NaN Holding BV.
  * All rights reserved.
  *
  * The Original Code is: all of this file.
@@ -36,8 +46,14 @@
 
 /*
  * $Log$
- * Revision 1.1  2002/10/12 11:37:45  hans
- * Initial revision
+ * Revision 1.2  2002/10/13 15:57:16  hans
+ * - mention the GPL the right way
+ * - extend NaN Holding copyright to its proper startup
+ *
+ * Revision 1.1.1.1  2002/10/12 11:37:45  hans
+ * For the open source Blender project we start with this fresh repository.
+ * These are the NaN 2.25 Blender sources, slightly cleaned up, and stripped
+ * from parts that could not be open sourced. Enjoy!
  *
  * Revision 1.4  2002/01/04 17:02:55  nzc
  * - Removed double mutex unlock from plain colour drawing func.
