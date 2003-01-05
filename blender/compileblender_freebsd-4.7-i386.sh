e18ce821ef7081488d62338ea023f2957020e286
h_xnan <h_xnan@954f8c5b-7b00-dc11-b283-0030488c597c>
2003-01-05 14:38:58 +0000

    A small script that sets some environment variables and then calls the NaN makefiles to build blender (intern/ and source/) Targetted at FreeBSD, but might work elsewhere too :)
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@304 954f8c5b-7b00-dc11-b283-0030488c597c

---
+++ blender/compileblender_freebsd-4.7-i386.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# $Id$
+# ***** BEGIN GPL/BL DUAL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. The Blender
+# Foundation also sells licenses for use in proprietary software under
+# the Blender License.  See http://www.blender.org/BL/ for information
+# about this.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# The Original Code is Copyright (C) 2002 by Hans Lambermont
+# All rights reserved.
+#
+# The Original Code is: all of this file.
+#
+# Contributor(s): none yet.
+#
+# ***** END GPL/BL DUAL LICENSE BLOCK *****
+#
+# FreeBSD make kickstart script.
+# install from ports: python, openal, jpeg, png, sdl, nspr
+
+rm -f /tmp/.nanguess
+export MAKE=gmake
+export NANBLENDERHOME=`pwd`
+export MAKEFLAGS="-w -I $NANBLENDERHOME/source --no-print-directory"
+export HMAKE="$NANBLENDERHOME/source/tools/hmake/hmake"
+
+$HMAKE -C intern/
+if [ $? -eq 0 ]; then
+	$HMAKE -C source/
+fi
+
