fa919a41dea53e6eb448746ad2f9107c5b23044c
sirdude <sirdude@954f8c5b-7b00-dc11-b283-0030488c597c>
2003-03-20 15:27:38 +0000

    Modified them to use: #!/usr/bin/env perl each was pointing at different locations for perl and everything else is using that so hey might as well be consistant ;)
    Kent
    
    
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@532 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/tools/cygwin/cl_wrapper.pl
+++ blender/source/tools/cygwin/cl_wrapper.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 # $Id$
 #
 # ***** BEGIN GPL/BL DUAL LICENSE BLOCK *****
--- blender/source/tools/cygwin/java_wrapper.pl
+++ blender/source/tools/cygwin/java_wrapper.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 # $Id$
 #
 # ***** BEGIN GPL/BL DUAL LICENSE BLOCK *****
