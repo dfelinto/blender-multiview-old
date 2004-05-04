f3cdc5912e3c17e8a038a720ecdd8de3e590e1d3
sirdude <sirdude@954f8c5b-7b00-dc11-b283-0030488c597c>
2004-05-04 17:18:26 +0000

    removed old examples since they are no longer used at all.
    Kent
    
    
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@2468 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/make/example_scripts/linux_autoconf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This is an example call to configure for 
-# a linux box.
-#
-../blender/configure --prefix=/usr/local/blender \
-        --with-ode=/usr/local \
-        --enable-openal
