9d092cc53b1fe88f11e43c56b8955dcc9a1e9380
intrr <intrr@954f8c5b-7b00-dc11-b283-0030488c597c>
2005-08-28 10:55:07 +0000

    Fix gameengine compilation, nobody else seems to care ;-)
    Thanks to Jorge Bernal.
    
    
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@5213 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/gameengine/GamePlayer/netscape/src/ketsji/KXH_ketsji_hooks.cpp
+++ blender/source/gameengine/GamePlayer/netscape/src/ketsji/KXH_ketsji_hooks.cpp
@@ -524,7 +524,7 @@ initialize_gameengine(ketsji_engine_data* k, struct BlendFileData * active_file)
 	if (k->kx_engine)
 	{	
 		k->converter = new
-			KX_BlenderSceneConverter(active_file->main,
+			KX_BlenderSceneConverter(active_file->main, 0,
 						 k->kx_engine);
 		
 		PyObject* dictionaryobject 
