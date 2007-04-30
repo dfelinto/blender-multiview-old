2f5992723aa3361dc2795167956f4ab299263383
artificer <artificer@954f8c5b-7b00-dc11-b283-0030488c597c>
2007-04-30 17:03:15 +0000

    Removed some unused flags accidentally included in the previous commit.
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@10623 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/makesdna/DNA_modifier_types.h
+++ blender/source/blender/makesdna/DNA_modifier_types.h
@@ -282,12 +282,10 @@ enum {
 #define MOD_WAVE_X      1<<1
 #define MOD_WAVE_Y      1<<2
 #define MOD_WAVE_CYCL   1<<3
-#define MOD_WAVE_VCOL   1<<4
-#define MOD_WAVE_TURB   1<<5
-#define MOD_WAVE_NORM   1<<6
-#define MOD_WAVE_NORM_X 1<<7
-#define MOD_WAVE_NORM_Y 1<<8
-#define MOD_WAVE_NORM_Z 1<<9
+#define MOD_WAVE_NORM   1<<4
+#define MOD_WAVE_NORM_X 1<<5
+#define MOD_WAVE_NORM_Y 1<<6
+#define MOD_WAVE_NORM_Z 1<<7
 
 typedef struct WaveModifierData {
 	ModifierData modifier;
