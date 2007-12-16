272dc42c0c669eb2686ad1f39f4e00ec286a49cc
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2007-12-16 05:25:52 +0000

    makefile should work, (lenfunc) from PySequenceMethods seems not to be available everywhere use (inquiry)  instead since blender uses this elsewhere
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@12905 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/python/api2_5x/layer_set.c
+++ blender/source/blender/python/api2_5x/layer_set.c
@@ -276,7 +276,7 @@ static int LayerSet_length(BPyLayerSetObject *self)
 
 static PyObject * LayerSet_contains(BPyLayerSetObject *self, PyObject *key);
 static PySequenceMethods LayerSet_as_sequence = {
-	(lenfunc)LayerSet_length,		/* sq_length */
+	(inquiry)LayerSet_length,		/* sq_length */
 	0,								/* sq_concat */
 	0,								/* sq_repeat */
 	0,								/* sq_item */
