1b36af9af26c41d73340c1f4310ba606586796a3
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2008-04-18 23:23:43 +0000

    reversing accidental change to epy_docgen.sh, show world shading Paper button when Blend is disabled, (own oversight in last commit).
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@14472 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/python/api2_2x/doc/epy_docgen.sh
+++ blender/source/blender/python/api2_2x/doc/epy_docgen.sh
@@ -1,29 +1,12 @@
-#!/bin/bash
 # epy_docgen.sh
 # generates blender python doc using epydoc
 # requires epydoc in your PATH.
 # run from the doc directory containing the .py files
-# usage: ./epy_docgen.sh
+# usage:  sh epy_docgen.sh
 
-# force posix locale so [A-Z]*.py glob uses ASCII ordering
-LC_COLLATE=POSIX
+# set posix locale so regex works properly for [A-Z]*.py
+LC_ALL=POSIX
 
-EPY_VER=$( epydoc --version | sed -e "s/^.* version //" )
-case $EPY_VER in
-    3*)
-        EPY_PARAMS="--top API_intro --name \"Blender\""
-        ;;
-
-    [12]*)
-        EPY_PARAMS="-t API_intro.py -n \"Blender\""
-        ;;
-
-    *)
-        echo ERROR: Unknown epydoc version
-        exit 1
-        ;;
-
-esac
-
-epydoc -o BPY_API --url "http://www.blender.org" -n "Blender" \
- --no-private --no-frames $EPY_PARAMS [A-Z]*.py
+epydoc -o BPY_API --url "http://www.blender.org" -t API_intro.py \
+ -n "Blender" --no-private --no-frames \
+$( ls [A-Z]*.py )
