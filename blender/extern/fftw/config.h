ba2a3278e68637fb47723e50333e87b460ee2983
genscher <genscher@954f8c5b-7b00-dc11-b283-0030488c597c>
2008-09-28 13:47:43 +0000

    Another msvc fix try III
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@16787 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/extern/fftw/config.h
+++ blender/extern/fftw/config.h
@@ -232,7 +232,7 @@
 #define HAVE_UINTPTR_T 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
+/* #undef HAVE_UNISTD_H */
 
 /* Define to 1 if you have the `vprintf' function. */
 #define HAVE_VPRINTF 1
