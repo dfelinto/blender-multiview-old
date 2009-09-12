abc722dec933b14eec5267653432799fe8e55ee2
theeth <theeth@954f8c5b-7b00-dc11-b283-0030488c597c>
2009-09-12 12:58:29 +0000

    removing 2.49 from trunk
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@23146 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/extern/ffmpeg/libavutil/intfloat_readwrite.d
+++
@@ -1,5 +0,0 @@
-/home/peter/cvs/blender/bf-blender/blender/extern/ffmpeg/libavutil/intfloat_readwrite.o : intfloat_readwrite.c common.h \
-  /usr/include/inttypes.h /usr/include/features.h \
-  /usr/include/sys/cdefs.h /usr/include/gnu/stubs.h /usr/include/stdint.h \
-  /usr/include/bits/wchar.h /usr/include/bits/wordsize.h \
-  intfloat_readwrite.h
--- blender/source/blender/.obj.Makefile
+++
@@ -1,9 +0,0 @@
-#
-# $Id$
-#
-
-SDIR = $(HOME)/develop/source/blender
-
-all debug clean:
-	@echo "****> Object Makefile, chdir to $(SDIR) ..."
-	@$(MAKE) -C $(SDIR) $@ || exit 1;
