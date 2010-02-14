cc00b02ccffc75751e1142cc719d75234260fa24
theeth <theeth@954f8c5b-7b00-dc11-b283-0030488c597c>
2010-02-14 19:01:15 +0000

    Removing ffmpeg dependencies from extern
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@26896 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/extern/ffmpeg/libavutil/crc.d
+++
@@ -1,3 +0,0 @@
-/home/peter/cvs/blender/bf-blender/blender/extern/ffmpeg/libavutil/crc.o : crc.c common.h /usr/include/inttypes.h /usr/include/features.h \
-  /usr/include/sys/cdefs.h /usr/include/gnu/stubs.h /usr/include/stdint.h \
-  /usr/include/bits/wchar.h /usr/include/bits/wordsize.h crc.h
