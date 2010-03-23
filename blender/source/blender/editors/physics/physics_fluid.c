fcbbc6fa629671e4609a4ccaa5d36bd8d7363521
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2010-03-23 14:09:09 +0000

    more lint includes Only source/blender/editors/ dir, should not give errors on different platforms Only removing:  UI_*.h, ED_*.h, WM_*.h, DNA_*.h, IMB_*.h, RNA_*.h, PIL_*.h
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@27676 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/editors/physics/physics_fluid.c
+++ blender/source/blender/editors/physics/physics_fluid.c
@@ -45,20 +45,7 @@
 #include "MEM_guardedalloc.h"
 
 /* types */
-#include "DNA_curve_types.h"
-#include "DNA_object_types.h"
 #include "DNA_object_fluidsim.h"	
-#include "DNA_key_types.h"
-#include "DNA_mesh_types.h"
-#include "DNA_meshdata_types.h"
-#include "DNA_lattice_types.h"
-#include "DNA_scene_types.h"
-#include "DNA_camera_types.h"
-#include "DNA_screen_types.h"
-#include "DNA_space_types.h"
-#include "DNA_userdef_types.h"
-#include "DNA_ipo_types.h"
-#include "DNA_key_types.h" 
 
 #include "BLI_blenlib.h"
 #include "BLI_threads.h"
@@ -81,16 +68,13 @@
 #include "BKE_scene.h"
 #include "BKE_softbody.h"
 
-#include "PIL_time.h"
 
 #include "LBM_fluidsim.h"
 
 #include "BIF_gl.h"
 
-#include "ED_fluidsim.h"
 #include "ED_screen.h"
 
-#include "WM_api.h"
 #include "WM_types.h"
 
 #include "physics_intern.h" // own include
