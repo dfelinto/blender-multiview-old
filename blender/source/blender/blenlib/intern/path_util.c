b66fcee3a5bae1991fac1f26ec5eba40a834a0b1
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2010-06-03 13:31:50 +0000

    search for scripts in the release directory relative to the blender binary.
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@29181 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/blenlib/intern/path_util.c
+++ blender/source/blender/blenlib/intern/path_util.c
@@ -844,6 +844,10 @@ static int gethome_path_local(char *targetpath, char *folder_name)
 	i = s - bprogname + 1;
 	BLI_strncpy(bprogdir, bprogname, i);
 
+	/* try release/folder_name (BIN relative) */
+	if(test_data_path(targetpath, bprogdir, "release", folder_name))
+		return 1;
+
 	/* try release/folder_name (CWD relative) */
 	if(test_data_path(targetpath, BLI_getwdN(cwd), "release", folder_name))
 		return 1;
