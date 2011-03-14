dfelinto/blender-multiview-old

commit 27e1ea26264e90eb42a4d79b91cca94200040020

py/rna, ability to have python static methods in collections.
git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@35492 954f8c5b-7b00-dc11-b283-0030488c597c

blender/source/blender/python/intern/bpy_rna.c
@@ -76,6 +76,7 @@
 #define USE_MATHUTILS
 #define USE_STRING_COERCE
 
+static PyObject* pyrna_struct_Subtype(PointerRNA *ptr);
 static PyObject *pyrna_prop_collection_values(BPy_PropertyRNA *self);
 
 int pyrna_struct_validity_check(BPy_StructRNA *pysrna)
@@ -3126,8 +3127,36 @@ static PyObject *pyrna_prop_collection_getattro(BPy_PropertyRNA *self, PyObject 
 		}
 	}
 
-	/* The error raised here will be displayed */
+#if 0
 	return PyObject_GenericGetAttr((PyObject *)self, pyname);
+#else
+	{
+		/* Could just do this except for 1 awkward case.
+		 * PyObject_GenericGetAttr((PyObject *)self, pyname);
+		 * so as to support 'bpy.data.library.load()'
+		 * note, this _only_ supports static methods */
+
+		PyObject *ret= PyObject_GenericGetAttr((PyObject *)self, pyname);
+
+		if(ret == NULL) {
+			/* since this is least common case, handle it last */
+			PointerRNA r_ptr;
+			PyObject *error_type, *error_value, *error_traceback;
+			PyErr_Fetch(&error_type, &error_value, &error_traceback);
+			PyErr_Clear();
+
+			if(RNA_property_collection_type_get(&self->ptr, self->prop, &r_ptr)) {
+				PyObject *cls= pyrna_struct_Subtype(&r_ptr); /* borrows */
+				ret= PyObject_GenericGetAttr(cls, pyname);
+				if(ret == NULL) {
+					PyErr_Restore(error_type, error_value, error_traceback);
+				}
+			}
+		}
+
+		return ret;
+	}
+#endif
 }
 
 //--------------- setattr-------------------------------------------

commit 06c240bd65a03bb1bfa37520f7a85656527e53d6

fix [#26489] Auto completion in console broken own mistake in r35492.
git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@35533 954f8c5b-7b00-dc11-b283-0030488c597c

blender/source/blender/python/intern/bpy_rna.c
@@ -3141,13 +3141,16 @@ static PyObject *pyrna_prop_collection_getattro(BPy_PropertyRNA *self, PyObject 
 		if(ret == NULL) {
 			/* since this is least common case, handle it last */
 			PointerRNA r_ptr;
-			PyObject *error_type, *error_value, *error_traceback;
-			PyErr_Fetch(&error_type, &error_value, &error_traceback);
-			PyErr_Clear();
-
 			if(RNA_property_collection_type_get(&self->ptr, self->prop, &r_ptr)) {
-				PyObject *cls= pyrna_struct_Subtype(&r_ptr); /* borrows */
+				PyObject *cls;
+
+				PyObject *error_type, *error_value, *error_traceback;
+				PyErr_Fetch(&error_type, &error_value, &error_traceback);
+				PyErr_Clear();
+
+				cls= pyrna_struct_Subtype(&r_ptr); /* borrows */
 				ret= PyObject_GenericGetAttr(cls, pyname);
+				/* restore the original error */
 				if(ret == NULL) {
 					PyErr_Restore(error_type, error_value, error_traceback);
 				}