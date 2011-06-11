4df2dce75457e3f250113121a555d58265dcdbe4
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2011-06-11 06:53:21 +0000

    error in last commit
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@37400 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/build_files/cmake/macros.cmake
+++ blender/build_files/cmake/macros.cmake
@@ -36,7 +36,7 @@ macro(blender_source_group
 	source_group("Source Files" FILES CMakeLists.txt)
 
 	foreach(_SRC ${sources})
-		get_filename_component(_SRC_EXT ${SRC} EXT)
+		get_filename_component(_SRC_EXT ${_SRC} EXT)
 		if(${_SRC_EXT} MATCHES ".h" OR ${_SRC_EXT} MATCHES ".hpp")
 			source_group("Header Files" FILES ${_SRC})
 		else()
