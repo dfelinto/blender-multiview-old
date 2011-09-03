8a16c926fa8e6c985d1a659469ee41773fb3a61a
campbellbarton <campbellbarton@954f8c5b-7b00-dc11-b283-0030488c597c>
2011-09-03 02:15:49 +0000

    whitespace edits
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@39883 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/gameengine/GamePlayer/xembed/npunix.c
+++ blender/source/gameengine/GamePlayer/xembed/npunix.c
@@ -56,7 +56,7 @@ static NPNetscapeFuncs   gNetscapeFuncs;	/* Netscape Function table */
 
 void
 NPN_Version(int* plugin_major, int* plugin_minor,
-	     int* netscape_major, int* netscape_minor)
+            int* netscape_major, int* netscape_minor)
 {
 	*plugin_major = NP_VERSION_MAJOR;
 	*plugin_minor = NP_VERSION_MINOR;
