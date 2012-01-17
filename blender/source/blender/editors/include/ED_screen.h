a5eeec4f03e2b64c97e94c850a296ff02f9924c5
psy-fi <psy-fi@954f8c5b-7b00-dc11-b283-0030488c597c>
2012-01-17 16:31:13 +0000

    Uv Tools branch GSOC 2011 ========================= Documentation: http://wiki.blender.org/index.php/User:Psy-Fi/UV_Tools
    Major features include:
    
    *16 bit image support in viewport
    *Subsurf aware unwrapping
    *Smart Stitch(snap/rotate islands, preview, middlepoint/endpoint stitching)
    *Seams from islands tool (marks seams and sharp, depending on settings)
    *Uv Sculpting(Grab/Pinch/Rotate)
    
    All tools are complete apart from stitching that is considered stable but with an extra edge mode under development(will be in soc-2011-onion-uv-tools).
    
    
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@43460 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/editors/include/ED_screen.h
+++ blender/source/blender/editors/include/ED_screen.h
@@ -169,6 +169,7 @@ int		ED_operator_editlattice(struct bContext *C);
 int		ED_operator_editmball(struct bContext *C);
 int		ED_operator_uvedit(struct bContext *C);
 int		ED_operator_uvmap(struct bContext *C);
+int		ED_operator_uv_seam_tag_check_active(struct bContext *C);
 int		ED_operator_posemode(struct bContext *C);
 
 
--- blender/source/blender/imbuf/IMB_imbuf.h
+++ blender/source/blender/imbuf/IMB_imbuf.h
@@ -371,6 +371,7 @@ void IMB_rect_from_float(struct ImBuf *ibuf);
 /* create char buffer for part of the image, color corrected if necessary,
    Changed part will be stored in buffer. This is expected to be used for texture painting updates */ 
 void IMB_partial_rect_from_float(struct ImBuf *ibuf, float *buffer, int x, int y, int w, int h);
+/* create a new float buffer containing color corrected colors from an Imbuf */
 void IMB_float_from_rect(struct ImBuf *ibuf);
 void IMB_float_from_rect_simple(struct ImBuf *ibuf); /* no profile conversion */
 /* note, check that the conversion exists, only some are supported */
