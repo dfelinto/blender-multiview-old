d3ec58c59a858543b81d94e514b9e70c490949c4
dingto <dingto@954f8c5b-7b00-dc11-b283-0030488c597c>
2013-06-01 02:21:29 +0000

    * Small syntax correct for last commit, sorry  4am here. ;)
    git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@57167 954f8c5b-7b00-dc11-b283-0030488c597c

--- blender/source/blender/render/intern/source/pipeline.c
+++ blender/source/blender/render/intern/source/pipeline.c
@@ -2270,7 +2270,7 @@ int RE_is_rendering_allowed(Scene *scene, Object *camera_override, ReportList *r
 		}
 
 #ifdef WITH_FREESTYLE
-		if (scene->r.mode & R_EDGE_FRS && (!BKE_scene_use_new_shading_nodes(scene))) {
+		if ((scene->r.mode & R_EDGE_FRS) && (!BKE_scene_use_new_shading_nodes(scene))) {
 			BKE_report(reports, RPT_ERROR, "Panoramic camera not supported in Freestyle");
 			return 0;
 		}
