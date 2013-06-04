235fc49d74f173872d6abfb6ee8cd3ca98babe2e
Dalai Felinto <dfelinto@gmail.com>
2013-06-04 11:14:39 -0700

    multiview: interlace code cleanup (no functional change)

--- source/blender/windowmanager/intern/wm_stereo.c
+++ source/blender/windowmanager/intern/wm_stereo.c
@@ -122,52 +122,54 @@ static void wm_method_draw_stereo_epilepsy(wmWindow *win)
 static GLuint left_interlace_mask[32];
 static GLuint right_interlace_mask[32];
 static int interlace_prev_type = -1;
-static char interlace_prev_swap_left_right = -1;
+static char interlace_prev_swap = -1;
 
 static void wm_interlace_create_masks(void)
 {
+	GLuint pattern;
+	char i;
 	char swap = U.stereo_flag & USER_INTERLACE_SWAP_LEFT_RIGHT;
-	if( (interlace_prev_type != U.interlace_type) || (interlace_prev_swap_left_right != swap) ) {
-		char i;
-		GLuint pattern;
-		switch(U.interlace_type) {
-			case USER_INTERLACE_TYPE_ROW_INTERLEAVED:
-				pattern = 0x00000000;
-				pattern = swap? ~pattern : pattern;
-				for(i = 0; i < 32; i += 2) {
-					left_interlace_mask[i] = pattern;
-					right_interlace_mask[i] = ~pattern;
-				}
-				for(i = 1; i < 32; i += 2) {
-					left_interlace_mask[i] = ~pattern;
-					right_interlace_mask[i] = pattern;
-				}
-				break;
-			case USER_INTERLACE_TYPE_COLUMN_INTERLEAVED:
-				pattern = 0x55555555;
-				pattern = swap? ~pattern : pattern;
-				for(i = 0; i < 32; i++) {
-					left_interlace_mask[i] = pattern;
-					right_interlace_mask[i] = ~pattern;
-				}
-				break;
-			case USER_INTERLACE_TYPE_CHECKERBOARD_INTERLEAVED:
-			default:
-				pattern = 0x55555555;
-				pattern = swap? ~pattern : pattern;
-				for(i = 0; i < 32; i += 2) {
-					left_interlace_mask[i] = pattern;
-					right_interlace_mask[i] = ~pattern;
-				}
-				for(i = 1; i < 32; i += 2) {
-					left_interlace_mask[i] = ~pattern;
-					right_interlace_mask[i] = pattern;
-				}
-				break;
-		}
-		interlace_prev_type = U.interlace_type;
-		interlace_prev_swap_left_right = swap;
+
+	if (interlace_prev_type == U.interlace_type && interlace_prev_swap == swap)
+		return;
+
+	switch(U.interlace_type) {
+		case USER_INTERLACE_TYPE_ROW_INTERLEAVED:
+			pattern = 0x00000000;
+			pattern = swap? ~pattern : pattern;
+			for(i = 0; i < 32; i += 2) {
+				left_interlace_mask[i] = pattern;
+				right_interlace_mask[i] = ~pattern;
+			}
+			for(i = 1; i < 32; i += 2) {
+				left_interlace_mask[i] = ~pattern;
+				right_interlace_mask[i] = pattern;
+			}
+			break;
+		case USER_INTERLACE_TYPE_COLUMN_INTERLEAVED:
+			pattern = 0x55555555;
+			pattern = swap? ~pattern : pattern;
+			for(i = 0; i < 32; i++) {
+				left_interlace_mask[i] = pattern;
+				right_interlace_mask[i] = ~pattern;
+			}
+			break;
+		case USER_INTERLACE_TYPE_CHECKERBOARD_INTERLEAVED:
+		default:
+			pattern = 0x55555555;
+			pattern = swap? ~pattern : pattern;
+			for(i = 0; i < 32; i += 2) {
+				left_interlace_mask[i] = pattern;
+				right_interlace_mask[i] = ~pattern;
+			}
+			for(i = 1; i < 32; i += 2) {
+				left_interlace_mask[i] = ~pattern;
+				right_interlace_mask[i] = pattern;
+			}
+			break;
 	}
+	interlace_prev_type = U.interlace_type;
+	interlace_prev_swap = swap;
 }
 
 static void wm_method_draw_stereo_interlace(wmWindow *win)
