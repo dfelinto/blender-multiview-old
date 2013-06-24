bf279d73b1f8be4ca97887581d54ba18e5d824e6
Alexey Akishin <alex@science.su>
2013-06-24 03:56:29 +0000

    multiview: Fix mistake in previous commit

--- release/scripts/startup/bl_ui/properties_render_layer.py
+++ release/scripts/startup/bl_ui/properties_render_layer.py
@@ -221,11 +221,11 @@ class RENDERLAYER_PT_view_options(RenderLayerButtonsPanel, Panel):
         col.prop(rv, "camera")
 
         col.separator()
-        col.prop(rv, "use_name_as_label")
+        col.prop(rv, "use_custom_suffix")
 
         sub = col.column()
-        sub.active = not rv.use_name_as_label
-        sub.prop(rv, "label")
+        sub.active = rv.use_custom_suffix
+        sub.prop(rv, "file_suffix")
 
 
 if __name__ == "__main__":  # only for live edit.
