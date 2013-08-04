dfelinto/blender-multiview-old

commit 99a00bd242948fc1a2dbe0fef8af625088e5ad14

Cleaning up some warnings that occur when an invalid navmesh is generated.
git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@58420 954f8c5b-7b00-dc11-b283-0030488c597c

blender/source/blender/blenkernel/intern/navmesh_conversion.c
@@ -440,6 +440,9 @@ int buildNavMeshDataByDerivedMesh(DerivedMesh *dm, int *vertsPerPoly,
 	int ntris = 0, *recastData = NULL;
 	unsigned short *tris = NULL;
 
+	/* Don't bother converting if there is nothing to convert */
+	if (!*nverts) return 0;
+
 	res = buildRawVertIndicesData(dm, nverts, verts, &ntris, &tris, trisToFacesMap, &recastData);
 	if (!res) {
 		printf("Converting navmesh: Error! Can't get vertices and indices from mesh\n");

blender/source/blender/blenkernel/intern/DerivedMesh.c
@@ -3103,7 +3103,7 @@ static DerivedMesh *navmesh_dm_createNavMeshForVisualization(DerivedMesh *dm)
 		}
 	}
 	else {
-		printf("%s: Error during creation polygon infos\n", __func__);
+		printf("Navmesh: Unable to generate valid Navmesh");
 	}
 
 	/* clean up */

commit 022d88f6dc3948fa9bcbc314e92c7473e120d0ac

fix navmesh, error was introduced by r58420 looks like changes weren't tested.
git-svn-id: https://svn.blender.org/svnroot/bf-blender/trunk@58890 954f8c5b-7b00-dc11-b283-0030488c597c

blender/source/blender/blenkernel/intern/navmesh_conversion.c
@@ -440,9 +440,6 @@ int buildNavMeshDataByDerivedMesh(DerivedMesh *dm, int *vertsPerPoly,
 	int ntris = 0, *recastData = NULL;
 	unsigned short *tris = NULL;
 
-	/* Don't bother converting if there is nothing to convert */
-	if (!*nverts) return 0;
-
 	res = buildRawVertIndicesData(dm, nverts, verts, &ntris, &tris, trisToFacesMap, &recastData);
 	if (!res) {
 		printf("Converting navmesh: Error! Can't get vertices and indices from mesh\n");