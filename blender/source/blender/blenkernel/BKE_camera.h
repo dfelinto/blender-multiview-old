1fe60f446248535fa687d76d38c01715420fcd14
Dalai Felinto <dfelinto@gmail.com>
2013-11-05 12:46:05 -0200

    multiview: handle advanced stereo setting

--- blender/source/blender/blenkernel/BKE_camera.h
+++ blender/source/blender/blenkernel/BKE_camera.h
@@ -121,7 +121,7 @@ void BKE_camera_view_frame(struct Scene *scene, struct Camera *camera, float r_v
 int BKE_camera_view_frame_fit_to_scene(struct Scene *scene, struct View3D *v3d, struct Object *camera_ob,
                                        float r_co[3]);
 
-struct Object *BKE_camera_multiview_advanced(struct RenderData *rd, struct Object *camera, const char *suffix);
+struct Object *BKE_camera_multiview_advanced(struct Scene *scene, struct RenderData *rd, struct Object *camera, const char *suffix);
 void BKE_camera_stereo_matrices(struct Object *camera, float viewmat[4][4], float *shift, bool left);
 
 #ifdef __cplusplus
